96ba14da8807d7ee689b5c01692d0bbf4b62d0da
mone <simonegraham42@gmail.com>
2024-01-08 21:02:26 -0500

    removed secrets

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from db_creds import cred
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:R0b0tcat3@localhost:3306/test_db_1"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://{name}:{password}@{server}/{database}".format(name=cred.name,password = cred.password ,server = cred.server,database = cred.database)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
@@ -9,3 +10,4 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
